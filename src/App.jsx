7027310d481bd2e48426c08f3a236f9be7d2d575
MauroDS <mauro.diaz.santilli@gmail.com>
2023-05-21 19:59:56 -0300

    Inicaindo ej7

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css'
+import EmpleadoList from './components/EmpleadoList';
+function App() {
+
+  return (
+    <>
+     <EmpleadoList/>
+    </>
+  )
+}
+
+export default App
